570b8aed85e1e788c03140e64006e0d59626788c
harsath <harsath@mm.st>
2022-11-18 17:31:02 -0500

    prism: add overrides to PRINTLN, PRINT and STRING

--- src/prism/PrintFunctionCallExpression.java
+++ src/prism/PrintFunctionCallExpression.java
@@ -2,10 +2,21 @@ package prism;
 
 public class PrintFunctionCallExpression implements Expression {
 	private Expression expr;
+	private boolean executed = false;
 
 	public PrintFunctionCallExpression(Expression expr) {
 		this.expr = expr;
 	}
+
+        @Override
+        public void setIsExecuted(boolean value) {
+                this.executed = value;
+        }
+
+        @Override
+        public boolean getIsExecuted() {
+                return this.executed;
+        }
 
 	public Expression getExpr() {
 		return expr;
