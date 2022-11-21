23541d8cf2b4850c6f9e306d81e21f0c0f8d499b
harsath <harsath@mm.st>
2022-11-21 15:03:30 -0500

    prism_coverage: fix code format

--- src/prism_coverage/PrismCodeCoverage.java
+++ src/prism_coverage/PrismCodeCoverage.java
@@ -60,12 +60,15 @@ public class PrismCodeCoverage {
         public Integer getTotalCoveredClassDeclarations() {
                 return total_covered_class_decls;
         }
-        
-        public Integer getTotalIfElse() { return total_if_else; }
 
-		public Integer getTotalCoveredIfElse() { return total_covered_if_else; }
-        
-        
+        public Integer getTotalIfElse() {
+                return total_if_else;
+        }
+
+        public Integer getTotalCoveredIfElse() {
+                return total_covered_if_else;
+        }
+
         private Integer getTotalStatementsFromFunction(FunctionDeclaration fn_decl) {
                 return fn_decl.getFunctionBody().getStatements().size();
         }
@@ -125,7 +128,7 @@ public class PrismCodeCoverage {
                                 }
                         } else if (decl instanceof FunctionDeclaration) {
                                 total_function_decls++;
-                                codeCoverageBlockCheck(((FunctionDeclaration)decl).getFunctionBody());
+                                codeCoverageBlockCheck(((FunctionDeclaration) decl).getFunctionBody());
                                 if (decl.getIsExecuted()) {
                                         total_covered_function_decls++;
                                 }
@@ -155,169 +158,175 @@ public class PrismCodeCoverage {
                         }
                 }
         }
-        
-        private void codeCoverageBlockCheck(BlockStatement block) { //For decision coverage
-        	List<Statement> statements = block.getStatements();
-        	
-        	for (Statement statement : statements) {
-        		if (statement instanceof IfElseStatement) {
-        			IfElseStatement if_cast = (IfElseStatement) statement;
-        			total_if_else++;
-        			if (if_cast.getElse_statement_block() != null) {
-        				total_if_else++;
-        			}
-        			if (if_cast.getIsExecuted()) {
-        				if (if_cast.getIf_statement_block().getIsExecuted()) { 
-        					total_covered_if_else++;
-        				}
-        				codeCoverageBlockCheck((BlockStatement) if_cast.getIf_statement_block());
-        			}
-        			if (if_cast.getElse_statement_block().getIsExecuted())
-        			{
-        				total_covered_if_else++;
-            			codeCoverageBlockCheck((BlockStatement) if_cast.getElse_statement_block());
-        			}
-        		}
-        	}
+
+        private void codeCoverageBlockCheck(BlockStatement block) { // For decision coverage
+                List<Statement> statements = block.getStatements();
+
+                for (Statement statement : statements) {
+                        if (statement instanceof IfElseStatement) {
+                                IfElseStatement if_cast = (IfElseStatement) statement;
+                                total_if_else++;
+                                if (if_cast.getElse_statement_block() != null) {
+                                        total_if_else++;
+                                }
+                                if (if_cast.getIsExecuted()) {
+                                        if (if_cast.getIf_statement_block().getIsExecuted()) {
+                                                total_covered_if_else++;
+                                        }
+                                        codeCoverageBlockCheck((BlockStatement) if_cast.getIf_statement_block());
+                                }
+                                if (if_cast.getElse_statement_block().getIsExecuted()) {
+                                        total_covered_if_else++;
+                                        codeCoverageBlockCheck((BlockStatement) if_cast.getElse_statement_block());
+                                }
+                        }
+                }
         }
+
         public String generateHTML() throws IOException {
-        	StringBuilder sb = new StringBuilder();
-            sb.append("<html>");
-            sb.append("<head>");
-            sb.append("<style>\n"
-            		+ "                        .color-red {\n"
-            		+ "                                color: red;\n"
-            		+ "                        }\n"
-            		+ "                        .covered {\n"
-            		+ "                                background-color: #50DBB4;\n"
-            		+ "                                margin-bottom: 0px;\n"
-            		+ "                                padding: 1px;\n"
-            		+ "								   white-space:pre;\n"			
-            		+ "                        }\n"
-            		+ "                        .not-covered {\n"
-            		+ "                                background-color: #FF6263;\n"
-            		+ "                                margin-bottom: 0px;\n"
-            		+ "                                padding: 1px;\n"
-            		+ "								   white-space:pre;\n"	
-            		+ "                        }\n"
-            		+"						   .tabbed {\n"
-            		+ "                                padding-left: 3em;\n"
-            		+ "							}\n	"			
-            		+ "                        #main-body {\n"
-            		+ "                                width: 70%;\n"
-            		+ "                        }\n"
-            		+ "                </style>");
-            sb.append("<title>Code Coverage Result");
-            sb.append("</title>");
-            sb.append("</head>");
-            sb.append("<body>");
-            sb.append("</body>");
-            sb.append("</html>");
-            
-            String input_file_name = new String();
-        	sb.append("<div id=\"header-text\">\n"
-        			+ "<p>Total Statements: <span class=\"color-red\">" + getTotalFunctionDeclarations() +"</span></p>\n"
-        			+ "<p>Total Statements Covered:<span class=\"color-red\">" +getTotalCoveredFunctionDeclarations()+ "</span></p>\n"
-        			+ "<p>Total Statement Coverage<code>"+input_file_name+"</code>: <span class=\"color-red\">"
-        			+ (int)calcCoveragePercentage(this.getTotalCoveredFunctionDeclarations(), this.getTotalFunctionDeclarations())+"%</span></p>\n"
-        			
-					+ "<p>Total Desicions : <span class=\"color-red\">" + getTotalIfElse() +"</span></p>\n"
-					+ "<p>Total Desicions Covered: <span class=\"color-red\">" +getTotalCoveredIfElse()+ "</span></p>\n");
-        	if (getTotalIfElse()> 0) {
-        		sb.append("<p>Total Desicion Coverage<code>"+input_file_name+"</code>: <span class=\"color-red\">"
-        				+ (int)calcCoveragePercentage(this.getTotalCoveredIfElse(), this.getTotalIfElse())+"%</span></p>\n </div>");
-        	}else {
-        		sb.append("<p>Total Desicion Coverage<code>"+input_file_name+"</code>: <span class=\"color-red\"> 100%</span></p>\n </div>");
-        	}
-					
-					
-        
-            String result = sb.toString();
-            List<Declaration> decls = prism_program.getProgram();
-            for (Declaration decl : decls) {
-            	if (decl.getIsExecuted()) {
-            		if (decl instanceof FunctionDeclaration) {
-            			FunctionDeclaration funcDecl = (FunctionDeclaration) decl;
-            			String funcSignature = funcDecl.functionSignature();
-            			
-            			result += "<p class=\"covered\">"+ funcSignature + " { </p>";
-            			result += statementsToString(funcDecl.getFunctionBody());
-            			result += "<div class=\"covered\">} </div>";
-            		}
-            	} else {
-            		result += "<p class=\"not-covered\">"+ decl.toString() +"</p>";
-            	}
-            }    	
-            return result;
+                StringBuilder sb = new StringBuilder();
+                sb.append("<html>");
+                sb.append("<head>");
+                sb.append("<style>\n" + "                        .color-red {\n"
+                                + "                                color: red;\n" + "                        }\n"
+                                + "                        .covered {\n"
+                                + "                                background-color: #50DBB4;\n"
+                                + "                                margin-bottom: 0px;\n"
+                                + "                                padding: 1px;\n"
+                                + "								   white-space:pre;\n" + "                        }\n"
+                                + "                        .not-covered {\n"
+                                + "                                background-color: #FF6263;\n"
+                                + "                                margin-bottom: 0px;\n"
+                                + "                                padding: 1px;\n"
+                                + "								   white-space:pre;\n" + "                        }\n"
+                                + "						   .tabbed {\n"
+                                + "                                padding-left: 3em;\n"
+                                + "							}\n	" + "                        #main-body {\n"
+                                + "                                width: 70%;\n" + "                        }\n"
+                                + "                </style>");
+                sb.append("<title>Code Coverage Result");
+                sb.append("</title>");
+                sb.append("</head>");
+                sb.append("<body>");
+                sb.append("</body>");
+                sb.append("</html>");
+
+                String input_file_name = new String();
+                sb.append("<div id=\"header-text\">\n" + "<p>Total Statements: <span class=\"color-red\">"
+                                + getTotalFunctionDeclarations() + "</span></p>\n"
+                                + "<p>Total Statements Covered:<span class=\"color-red\">"
+                                + getTotalCoveredFunctionDeclarations() + "</span></p>\n"
+                                + "<p>Total Statement Coverage<code>" + input_file_name
+                                + "</code>: <span class=\"color-red\">"
+                                + (int) calcCoveragePercentage(this.getTotalCoveredFunctionDeclarations(),
+                                                this.getTotalFunctionDeclarations())
+                                + "%</span></p>\n"
+
+                                + "<p>Total Desicions : <span class=\"color-red\">" + getTotalIfElse() + "</span></p>\n"
+                                + "<p>Total Desicions Covered: <span class=\"color-red\">" + getTotalCoveredIfElse()
+                                + "</span></p>\n");
+                if (getTotalIfElse() > 0) {
+                        sb.append("<p>Total Desicion Coverage<code>" + input_file_name
+                                        + "</code>: <span class=\"color-red\">"
+                                        + (int) calcCoveragePercentage(this.getTotalCoveredIfElse(),
+                                                        this.getTotalIfElse())
+                                        + "%</span></p>\n </div>");
+                } else {
+                        sb.append("<p>Total Desicion Coverage<code>" + input_file_name
+                                        + "</code>: <span class=\"color-red\"> 100%</span></p>\n </div>");
+                }
+
+                String result = sb.toString();
+                List<Declaration> decls = prism_program.getProgram();
+                for (Declaration decl : decls) {
+                        if (decl.getIsExecuted()) {
+                                if (decl instanceof FunctionDeclaration) {
+                                        FunctionDeclaration funcDecl = (FunctionDeclaration) decl;
+                                        String funcSignature = funcDecl.functionSignature();
+
+                                        result += "<p class=\"covered\">" + funcSignature + " { </p>";
+                                        result += statementsToString(funcDecl.getFunctionBody());
+                                        result += "<div class=\"covered\">} </div>";
+                                }
+                        } else {
+                                result += "<p class=\"not-covered\">" + decl.toString() + "</p>";
+                        }
+                }
+                return result;
         }
-        
+
         public void writeHTMLOutput(String file_path) throws IOException {
-        	String body = generateHTML();
-            
-            FileWriter fstream ;
-            fstream = new FileWriter(file_path);
-        	
-        	BufferedWriter out = new BufferedWriter(fstream);
-        	
-        	out.write(body);
-        	out.close();
+                String body = generateHTML();
+
+                FileWriter fstream;
+                fstream = new FileWriter(file_path);
+
+                BufferedWriter out = new BufferedWriter(fstream);
+
+                out.write(body);
+                out.close();
         }
 
         public String statementsToString(BlockStatement statements) {
-        	String returner = new String();
-        	
-        	if (statements.getStatements().size() > 0) { //If there are statements in function  block
-				for (Statement statement :statements.getStatements()) {
-					if (statement instanceof IfElseStatement) {
-						IfElseStatement if_cast = (IfElseStatement) statement;
-	        			if (if_cast.getIsExecuted()) {
-	        				returner += "<div class=\"covered\">";
-	        				returner += "<div class=\"tabbed\">IF ("+ if_cast.getExpr_condition() +") { </br>" + "</div>";
-	        				
-	        				returner += statementsToString((BlockStatement)if_cast.getIf_statement_block());
-	        				returner += "</div>";
-	        			} else {
-	        				returner += "<div class=\"not-covered\">";
-	        				returner += "<div class=\"tabbed\">" + statement.toString() + "</div>";
-	        				returner += "</div>";
-	        			}
-	        			returner += "<div class=\"covered\">";
-	        			returner += "<div class=\"tabbed\">} </div>";
-	        			returner += "</div>";
-	        			if (if_cast.getElse_statement_block().getIsExecuted()) {
-	        				returner += "<div class=\"covered\">";
-	        				returner += "<div class=\"tabbed\">ELSE { </br>"+ if_cast.getElse_statement_block() +"</br>}</br>" + "</div>";
-	        				returner += "</div>";
-	        				//returner += statementsToString((BlockStatement)if_cast.getElse_statement_block());
-	        			} else {
-	        				returner += "<div class=\"not-covered\">";
-	        				returner += "<div class=\"tabbed\">ELSE { </br>"+ if_cast.getElse_statement_block() +"</br>}</br>" + "</div>";
-	        				returner += "</div>";
+                String returner = new String();
 
-	        			}
-	        			
-					}else {
-						
-						if (statement.getIsExecuted()) {
-							returner += "<div class=\"covered\">";
-	        				returner += "<div class=\"tabbed\">	" + statement.toString() + "</div>";
-	        				returner += "</div>";
-	        			} else {
-	        				returner += "<div class=\"not-covered\">";
-	        				returner += "<div class=\"tabbed\">	" + statement.toString() + "</br></div>";
-	        				returner += "</div>";
-	        			}
-					}
-				}
-        	}
-        	return returner;
+                if (statements.getStatements().size() > 0) { // If there are statements in function block
+                        for (Statement statement : statements.getStatements()) {
+                                if (statement instanceof IfElseStatement) {
+                                        IfElseStatement if_cast = (IfElseStatement) statement;
+                                        if (if_cast.getIsExecuted()) {
+                                                returner += "<div class=\"covered\">";
+                                                returner += "<div class=\"tabbed\">IF (" + if_cast.getExpr_condition()
+                                                                + ") { </br>" + "</div>";
+
+                                                returner += statementsToString(
+                                                                (BlockStatement) if_cast.getIf_statement_block());
+                                                returner += "</div>";
+                                        } else {
+                                                returner += "<div class=\"not-covered\">";
+                                                returner += "<div class=\"tabbed\">" + statement.toString() + "</div>";
+                                                returner += "</div>";
+                                        }
+                                        returner += "<div class=\"covered\">";
+                                        returner += "<div class=\"tabbed\">} </div>";
+                                        returner += "</div>";
+                                        if (if_cast.getElse_statement_block().getIsExecuted()) {
+                                                returner += "<div class=\"covered\">";
+                                                returner += "<div class=\"tabbed\">ELSE { </br>"
+                                                                + if_cast.getElse_statement_block() + "</br>}</br>"
+                                                                + "</div>";
+                                                returner += "</div>";
+                                                // returner +=
+                                                // statementsToString((BlockStatement)if_cast.getElse_statement_block());
+                                        } else {
+                                                returner += "<div class=\"not-covered\">";
+                                                returner += "<div class=\"tabbed\">ELSE { </br>"
+                                                                + if_cast.getElse_statement_block() + "</br>}</br>"
+                                                                + "</div>";
+                                                returner += "</div>";
+
+                                        }
+
+                                } else {
+
+                                        if (statement.getIsExecuted()) {
+                                                returner += "<div class=\"covered\">";
+                                                returner += "<div class=\"tabbed\">	" + statement.toString() + "</div>";
+                                                returner += "</div>";
+                                        } else {
+                                                returner += "<div class=\"not-covered\">";
+                                                returner += "<div class=\"tabbed\">	" + statement.toString()
+                                                                + "</br></div>";
+                                                returner += "</div>";
+                                        }
+                                }
+                        }
+                }
+                return returner;
         }
 
-        
         public double calcCoveragePercentage(Integer covered, Integer total) {
-        	return ((double)covered / (double)total) * 100;
+                return ((double) covered / (double) total) * 100;
         }
-        
-        
-        
+
 }
