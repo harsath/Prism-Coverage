14c0b7301d31673202b3a1fcb223f586c4fab869
aiafrat8 <70404064+aiafrat8@users.noreply.github.com>
2022-12-06 16:00:02 -0500

    added html outputs and created new dir with tests to conform to the requirements from jackie (#55)
    * added html outputs and created new dir with tests to conform to the
    structure which is asked
    
    * ajusted test runner for location of test runners
    
    * Acutally fixed now
    
    Co-authored-by: anth <anth@Ubuntu.myguest.virtualbox.org>

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -4,9 +4,9 @@ BIN_OUT="classes"
 
 export CLASSPATH=".:${PWD}/lib/antlr-4.11.1-complete.jar"
 mkdir $BIN_OUT
-javac -d $BIN_OUT -cp lib/antlr-4.11.1-complete.jar:lib/junit-4.13.2.jar src/antlr/*.java src/prism/*.java src/prism_interpreter/*.java src/prism_coverage/*.java src/app/*.java src/tests/*.java
+javac -d $BIN_OUT -cp lib/antlr-4.11.1-complete.jar:lib/junit-4.13.2.jar src/antlr/*.java src/prism/*.java src/prism_interpreter/*.java src/prism_coverage/*.java src/app/*.java src/test_runner/*.java
 cd $BIN_OUT
-java tests.TestRunner
+java test_runner.TestRunner
 EXIT_CODE=$?
 if [ $EXIT_CODE -eq 0 ]; then
 	echo "Tests ran successfully"
