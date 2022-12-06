5cddd8cc57870ae8b347f52b218e9dc4665a0500
anth <anth@Ubuntu.myguest.virtualbox.org>
2022-12-06 15:20:54 -0500

    ajusted test runner for location of test runners

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -4,7 +4,7 @@ BIN_OUT="classes"
 
 export CLASSPATH=".:${PWD}/lib/antlr-4.11.1-complete.jar"
 mkdir $BIN_OUT
-javac -d $BIN_OUT -cp lib/antlr-4.11.1-complete.jar:lib/junit-4.13.2.jar src/antlr/*.java src/prism/*.java src/prism_interpreter/*.java src/prism_coverage/*.java src/app/*.java src/tests/*.java
+javac -d $BIN_OUT -cp lib/antlr-4.11.1-complete.jar:lib/junit-4.13.2.jar src/antlr/*.java src/prism/*.java src/prism_interpreter/*.java src/prism_coverage/*.java src/app/*.java src/test_runner/*.java
 cd $BIN_OUT
 java tests.TestRunner
 EXIT_CODE=$?
